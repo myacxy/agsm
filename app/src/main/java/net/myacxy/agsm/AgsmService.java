3d03f29cb90c3811c36ac9b8e77281aa1a770433
myacxy <myacxy@gmail.com>
2015-08-22 17:33:08 +0200

    actually update servers via service

--- app/src/main/java/net/myacxy/agsm/AgsmService.java
+++ app/src/main/java/net/myacxy/agsm/AgsmService.java
@@ -6,12 +6,23 @@ import android.os.*;
 import android.support.annotation.Nullable;
 
 import net.myacxy.agsm.interfaces.DatabaseManager;
+import net.myacxy.agsm.interfaces.GameFinder;
+import net.myacxy.agsm.interfaces.OnServerUpdatedListener;
+import net.myacxy.agsm.interfaces.ServerFinder;
 import net.myacxy.agsm.interfaces.ServerManager;
 import net.myacxy.agsm.managers.ActiveDatabaseManager;
 import net.myacxy.agsm.managers.JgsqServerManager;
+import net.myacxy.agsm.models.GameServerEntity;
+import net.myacxy.agsm.utils.ActiveServerFinder;
+import net.myacxy.agsm.utils.JgsqGameFinder;
+import net.myacxy.jgsq.helpers.ServerResponseStatus;
+import net.myacxy.jgsq.models.Game;
+import net.myacxy.jgsq.models.GameServer;
 
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EService;
+
+import java.util.List;
 
 import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 
@@ -23,22 +34,26 @@ public class AgsmService extends Service
     public static final int UPDATE_REASON_MANUAL = 0;
     public static final int UPDATE_REASON_PERIODIC = 1;
 
-    @Bean(JgsqServerManager.class) ServerManager serverManager;
-    @Bean(ActiveDatabaseManager.class) DatabaseManager databaseManager;
+    @Bean(JgsqServerManager.class)      ServerManager serverManager;
+    @Bean(ActiveDatabaseManager.class)  DatabaseManager databaseManager;
+    @Bean(ActiveServerFinder.class)     ServerFinder serverFinder;
+    @Bean(JgsqGameFinder.class)         GameFinder gameFinder;
 
     private Looper looper;
-    private ServiceHandler serviceHandler;
+    private AgsmServiceUpdateServersHandler serviceHandler;
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         HandlerThread thread = new HandlerThread("AgsmService", THREAD_PRIORITY_BACKGROUND);
         thread.start();
         looper = thread.getLooper();
-        serviceHandler = new ServiceHandler(looper);
+        serviceHandler = new AgsmServiceUpdateServersHandler(looper);
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
         Message message = serviceHandler.obtainMessage();
         message.arg1 = startId;
         serviceHandler.sendMessage(message);
@@ -51,18 +66,67 @@ public class AgsmService extends Service
         return null;
     }
 
-    private class ServiceHandler extends Handler {
+    private class AgsmServiceUpdateServersHandler extends Handler implements OnServerUpdatedListener
+    {
+        private List<GameServerEntity>      gameServerEntities;
+        private int count = 0;
 
-        public ServiceHandler(Looper looper)
+        public AgsmServiceUpdateServersHandler(Looper looper)
         {
             super(looper);
         }
 
         @Override
-        public void handleMessage(Message msg) {
-            // TODO update servers
-            System.out.println("stopping service " + msg.arg1);
-            stopSelf(msg.arg1);
+        public void handleMessage(Message msg)
+        {
+            gameServerEntities = serverFinder.findAll();
+            if(gameServerEntities != null)
+            {
+                count = gameServerEntities.size();
+                for (GameServerEntity gameServerEntity : gameServerEntities)
+                {
+                    refreshServer(gameServerEntity);
+                }
+            }
+            else
+            {
+                count = 0;
+                stopSelf();
+            }
+        }
+
+        @Override
+        public void onServerUpdated(GameServer gameServer)
+        {
+            if (gameServer.getProtocol().getResponseStatus() == ServerResponseStatus.OK)
+            {
+                databaseManager.update(gameServer);
+            }
+            else
+            {
+                System.out.println(gameServer.getProtocol().getResponseStatus().toString());
+            }
+            if(--count == 0)
+            {
+                onAllServerUpdated();
+            }
+        }
+
+        private void onAllServerUpdated()
+        {
+            stopSelf();
+        }
+
+        private void refreshServer(GameServerEntity gameServerEntity)
+        {
+            Game game = gameFinder.find(gameServerEntity.game.name);
+
+            serverManager.update(
+                    game,
+                    gameServerEntity.ipAddress,
+                    gameServerEntity.port,
+                    this
+            ); // update
         }
     }
-}
+} // AgsmService
